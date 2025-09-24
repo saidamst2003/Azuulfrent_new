fbb321b0badd64810b3b170c22d7d7718c32dbae
saidamst2003 <saida2003mst@gmail.com>
2025-09-24 17:34:37 +0100

    j'ai  creer page home

--- frent/src/app/app.ts
+++ frent/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
+import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
