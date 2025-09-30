0828f4a46cb4a2a231f2a2776b4da710301d4ecd
saidamst2003 <saida2003mst@gmail.com>
2025-10-01 00:20:41 +0100

    j' ai creer fonction post dans ts et j'ai importer FormBuilder

--- frent/src/app/atelier/atelier.ts
+++ frent/src/app/atelier/atelier.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtelierService } from '../service/atelierService';
 import { AtelierModel } from '../model/atelierModel.model';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-atelier',
   standalone: true,
@@ -11,23 +11,33 @@ import { AtelierModel } from '../model/atelierModel.model';
   styleUrls: ['./atelier.css'],
 })
 export class AtelierComponent implements OnInit {
+
   ateliers: AtelierModel[] = [];
+
+  postAtelier! : FormGroup;
+
   loading: boolean = true;
 
-  constructor(
-    private atelierService: AtelierService,
-    private cd: ChangeDetectorRef
-  ) {}
+  constructor( private atelierService: AtelierService, private cd: ChangeDetectorRef , private fb :FormBuilder) {}
 
   ngOnInit(): void {
     this.getAllAtelier();
+
+    this.postAtelier = this.fb.group({
+      nom: [null, Validators.required],
+      description: [null, Validators.required],
+      date: [null, Validators.required],
+      heure: [null, Validators.required],
+      categorie: [null, Validators.required]
+    });
   }
+  
 
   getAllAtelier(): void {
     this.atelierService.getAllAtelier().subscribe({
 
       next: (data) => {
-        
+
         this.ateliers = data;
         this.loading = false;
         this.cd.detectChanges(); 
@@ -40,4 +50,6 @@ export class AtelierComponent implements OnInit {
       }
     });
   }
+
+
 }
